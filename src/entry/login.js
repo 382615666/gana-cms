3058721a38f1345240926af75c0507de3c0c94bf
王湘赣 <wangxianggan@hnjing.com>
2018-03-01 16:01:38 +0800

    a

--- src/entry/login.js
+++ src/entry/login.js
@@ -15,6 +15,8 @@ class Login extends React.Component {
         super(props)
         this.state = {
             form: {
+                account: 'admin',
+                password: '123456'
             },
             selectName: '',
             rules: {
@@ -47,7 +49,9 @@ class Login extends React.Component {
         this.refs.login.validate(async valid => {
             if (valid) {
                 let res = await axios.get(`/login/${this.state.form.account}/${this.state.form.password}`)
-
+                if (!res.data.err) {
+                    window.location.href="/index.html"
+                }
             }
         })
     }
@@ -58,10 +62,10 @@ class Login extends React.Component {
                     <img src={logo} alt="" className="logo"/>
                     <Form ref="login" model={this.state.form} rules={this.state.rules}>
                         <Form.Item prop="account">
-                            <Input prepend="账号" placeholder="请输入您的账号" onSelect={() => this.onSelect('account')} onChange={this.onChange}></Input>
+                            <Input prepend="账号" value={this.state.form.account} placeholder="请输入您的账号" onSelect={() => this.onSelect('account')} onChange={this.onChange}></Input>
                         </Form.Item>
                         <Form.Item prop="password">
-                            <Input prepend="密码" type="password" placeholder="请输入您的密码" onSelect={() => this.onSelect('password')} onChange={this.onChange}></Input>
+                            <Input prepend="密码" value={this.state.form.password} type="password" placeholder="请输入您的密码" onSelect={() => this.onSelect('password')} onChange={this.onChange}></Input>
                         </Form.Item>
                     </Form>
                     <Button className="login-btn" type="primary" onClick={this.login}>立即登录</Button>
